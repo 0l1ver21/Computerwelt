fc24c6bc235f8a64ff87beda1c312142159d2e84
0l1ver21 <104281321+0l1ver21@users.noreply.github.com>
2022-06-01 00:21:46 -0500

    Computerwelt

---
+++ main.js
@@ -0,0 +1,17 @@
+$ (document).ready(function (){
+	$ ('ul.menu li a:first').addClass('active');
+	$ ('.secciones article').hide();
+	$ ('.secciones article:first').show();
+
+
+	$ ('ul.menu li a').click(function(){
+		$ ('ul.menu li a').removeClass('active');
+		$ (this).addClass('active');
+		$ ('.secciones article').hide();
+
+		
+		var activeTab = $ (this).attr('href');
+		$ (activeTab).show();
+		return false;
+	});
+});
